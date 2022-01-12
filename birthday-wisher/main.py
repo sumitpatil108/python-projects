f6b080bb4e447f8d399fdfdc4f2e3534fcb176aa
sumitpatil108 <69338434+sumitpatil108@users.noreply.github.com>
2022-01-12 17:43:56 +0530

    Update main.py

--- birthday-wisher/main.py
+++ birthday-wisher/main.py
@@ -1,9 +1,4 @@
-#To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
+
 
 
 from datetime import datetime
